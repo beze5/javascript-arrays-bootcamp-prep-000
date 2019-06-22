91c7693388dd2eac3d67ab3316cb35794b6f94b4
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-22 12:46:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
+var chocolateBars = new Array("snickers", "hundred grand", "kitkat", "skittles")
 
 function addElementToBeginningOfArray(testArray, beginningElement){
   const newArray = [beginningElement...testArray]
