657b52d84c2afbefe7008bbbbc10c175678ab8ad
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-22 14:23:51 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -85,7 +85,7 @@ function destructivelyRemoveElementFromEndOfArray(thisArray){
 }
 
 function removeElementFromEndOfArray(array){
-  array.slice(0, array.length - 2);
+  array.slice(0, array.length - 0);
 }
 
 
