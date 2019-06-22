6e78aaeacd532bbab1e95d3ac69b041d19935a2a
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-22 14:29:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -68,8 +68,8 @@ function removeElementFromBeginningOfArray(array){
 
 
 function removeElementFromBeginningOfArray(array){
-  var newArray = array.slice(0);
-  return newArray;
+  array.slice(0);
+  return array;
 }
 
 function removeElementFromBeginningOfArray(array){
