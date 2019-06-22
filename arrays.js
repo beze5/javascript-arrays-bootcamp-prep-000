486e316dee28ceeba8f9d0d1d90f34176520996f
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-22 13:06:53 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,14 @@ entrepreneurs[9];
 function accessElementInArray(array, index){
   return array[index];
 }
+
+// these are notes on removing an element from an array 
+
+const days = ["Monday", "Tuesday", "Wednesday"];
+
+days.shift(); // this removes the first element in the array (Monday)
+
+days; // this returns the array without the removed (first) element, Monday
+
+destructivelyRemoveElementFromBeginningOf
+
