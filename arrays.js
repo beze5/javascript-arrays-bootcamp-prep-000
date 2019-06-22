3e629cc24fe0661412073a91301e7490a9a398b7
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-22 13:01:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -12,6 +12,16 @@ function destructivelyAddElementToBeginningOfArray(testArray, beginningElement){
   return testArray
 }
 
+//these are notes on selecting and using specific elements from arrays
+
 var entrepreneurs = ["Oprah Winfrey", "Laurene Powell Jobs", "Arianna Huffington"];
 
 console.log(entrepreneurs[0]);
+
+var bio = " is the co-founder and editress-in-chief of The Huffington Post";
+console.log(entrepreneurs[2] + bio);
+
+entrepreneurs[9];
+
+function accessElementInArray(array, index){
+  return array
