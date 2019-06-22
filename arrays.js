2822f13e6ab4380cf0172d0c0371a362460508f6
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-22 14:20:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,6 +7,11 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 // ADD TO BEGINNING OF ARRAY
 function addElementToBeginningOfArray(array, item){
+  var newArray = [...array, item]
+  return newArray
+}
+
+function addElementToBeginningOfArray(array, item){
   var newArray = array;
   newArray.unshift(item);
   return newArray;
